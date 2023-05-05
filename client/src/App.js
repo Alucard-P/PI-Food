23a7b252bbb308ba9aa32a76ff123aeeb79c08a3
Alucard-P <paulmct93@gmail.com>
2023-05-05 14:15:22 -0500

    create an error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Route, useLocation } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Detail from "./Components/Detail/Detail";
 import Home from "./Components/Home/Home";
 import Lobby from "./Components/Lobby/Lobby";
 import Create from "./Components/Create/Create";
 import NavBar from "./Components/NavBar/NavBar";
+import Error from "./Components/ErrorPage/Error";
 import axios from "axios";
 // axios.defaults.baseURL = "https://pi-food-production-8b61.up.railway.app/";
 axios.defaults.baseURL = "http://localhost:3001/";
@@ -13,11 +14,14 @@ function App() {
   const location = useLocation();
   return (
     <div className="App">
-      {location.pathname === "/" ? null : <NavBar />}
-      <Route path={"/create"} component={Create} />
-      <Route path={"/home"} component={Home} />
-      <Route exact path={"/"} component={Lobby} />
-      <Route path={"/detail/:id"} component={Detail} />
+      <Switch>
+        {/* {location.pathname === "/" ? null : <NavBar />} */}
+        <Route path={"/create"} component={Create} />
+        <Route path={"/home"} component={Home} />
+        <Route exact path={"/"} component={Lobby} />
+        <Route path={"/detail/:id"} component={Detail} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import style from "./Home.module.css";
+import NavBar from "../NavBar/NavBar";
 import RecipeContainer from "../RecipeContainer/RecipeContainer";
 
 const Home = () => {
   return (
     <div className={style.container}>
       {/* <h1>soy el componente Home</h1> */}
+      <NavBar />
       <RecipeContainer />
     </div>
   );
